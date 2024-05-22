c1744331d3673d67561c39981a36e48d6f69dd4c
Ergi Lama <ergilana98@gmail.com>
2024-05-22 17:14:56 +0200

    Ready for deployment

--- my-app/app/career/page.tsx
+++ my-app/app/career/page.tsx
@@ -14,27 +14,11 @@ const Career = () => {
           </div>
           <div className={styles.textContainer}>
             <Card data={careerData} />
-            {/* <h1 className={styles.heading}>My Career</h1>
-            <p className={styles.text}>
-              Embark on a journey through the illustrious career of Ergi, a
-              seasoned software engineer whose expertise knows no bounds. From
-              the inception of his career, Ergi has been driven by an insatiable
-              curiosity for technology and an unwavering commitment to
-              excellence.
-            </p> */}
           </div>
         </div>
         <div className={styles.sections}>
           <div className={styles.textContainer}>
             <Card data={dreamsData} />
-            {/* <h1 className={styles.heading}>My Dreams</h1>
-            <p className={styles.text}>
-              In the tapestry of my aspirations, the thread of success weaves
-              through every fiber of my being. Each day is a step towards the
-              realization of my dreams, a testament to my unwavering
-              determination. Challenges become mere stepping stones,
-              opportunities in disguise, as I relentlessly pursue excellence.
-            </p> */}
           </div>
           <div className={styles.imageContainer}>
             <img className={styles.image} src="/book.svg" alt="bg" />
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,8 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
-import Card from "./components/Card/Card";
 import Presentation from "./components/Presentation/Presentation";
 import styles from "./page.module.css";
-import Link from "next/link";
 
 export default function Home() {
   return (
